185dbb05ee84c97fc668054c0f01a4e2d7a352e7
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-05-02 16:43:55 +0100

    Implement REST endpoint for Notification Settings (#10)

---
+++ commons-api/src/main/java/org/exoplatform/commons/api/notification/rest/model/ChannelActivationChoice.java
@@ -0,0 +1,52 @@
+package org.exoplatform.commons.api.notification.rest.model;
+
+public class ChannelActivationChoice {
+
+  private String  channelId;
+
+  private String  pluginId;
+
+  private boolean active;
+
+  private boolean channelActive;
+
+  public ChannelActivationChoice(String channelId, String pluginId, boolean active, boolean channelActive) {
+    this.channelId = channelId;
+    this.pluginId = pluginId;
+    this.active = active;
+    this.channelActive = channelActive;
+  }
+
+  public String getChannelId() {
+    return channelId;
+  }
+
+  public void setChannelId(String channelId) {
+    this.channelId = channelId;
+  }
+
+  public String getPluginId() {
+    return pluginId;
+  }
+
+  public void setPluginId(String pluginId) {
+    this.pluginId = pluginId;
+  }
+
+  public boolean isActive() {
+    return active;
+  }
+
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+
+  public boolean isChannelActive() {
+    return channelActive;
+  }
+
+  public void setChannelActive(boolean channelActive) {
+    this.channelActive = channelActive;
+  }
+
+}
--- commons-api/src/test/java/org/exoplatform/commons/api/notification/plugin/NotificationPluginUtilsTest.java
+++ commons-api/src/test/java/org/exoplatform/commons/api/notification/plugin/NotificationPluginUtilsTest.java
@@ -1,21 +1,16 @@
 package org.exoplatform.commons.api.notification.plugin;
 
-import junit.framework.TestCase;
+import static org.exoplatform.commons.api.notification.plugin.NotificationPluginUtils.BRANDING_COMPANY_NAME_SETTING_KEY;
+
+import org.mockito.Mockito;
 
 import org.exoplatform.commons.api.settings.SettingService;
 import org.exoplatform.commons.api.settings.SettingValue;
 import org.exoplatform.commons.api.settings.data.Context;
 import org.exoplatform.commons.api.settings.data.Scope;
-import org.exoplatform.container.ExoContainerContext;
-import org.exoplatform.container.PortalContainer;
-import org.exoplatform.services.organization.OrganizationService;
-import org.exoplatform.services.organization.UserProfile;
-import org.exoplatform.services.organization.UserProfileHandler;
+import org.exoplatform.component.test.*;
+import org.exoplatform.services.organization.*;
 import org.exoplatform.services.organization.impl.UserProfileImpl;
-import org.mockito.Mockito;
-
-import static org.exoplatform.commons.api.notification.plugin.NotificationPluginUtils.BRANDING_COMPANY_NAME_SETTING_KEY;
-
 
 /**
  * Created by eXo Platform SAS.
@@ -24,23 +19,43 @@ import static org.exoplatform.commons.api.notification.plugin.NotificationPlugin
  * @since 15/02/18 10:23
  */
 
-public class NotificationPluginUtilsTest extends TestCase {
+@ConfiguredBy(
+  {
+      @ConfigurationUnit(scope = ContainerScope.ROOT, path = "conf/configuration.xml"),
+      @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/portal/configuration.xml"),
+  }
+)
+public class NotificationPluginUtilsTest extends AbstractKernelTest {
+
+  public NotificationPluginUtilsTest() {
+    setForceContainerReload(true);
+  }
 
   public void testGetLanguage() throws Exception {
-    OrganizationService organizationService = PortalContainer.getInstance().getComponentInstanceOfType(OrganizationService.class);
-    String userName = "testUser";
+    OrganizationService organizationService = getContainer().getComponentInstanceOfType(OrganizationService.class);
+    String userName = "john";
     String langFR = "fr";
     String langFR_FR = "fr_FR";
     UserProfileHandler profileHandler = organizationService.getUserProfileHandler();
     UserProfile profile = new UserProfileImpl(userName);
-    profile.setAttribute("user.language",langFR);
-    profileHandler.saveUserProfile(profile,true);
-    
+    profile.setAttribute("user.language", langFR);
+    begin();
+    try {
+      profileHandler.saveUserProfile(profile, true);
+    } finally {
+      end();
+    }
+
     String language = NotificationPluginUtils.getLanguage(userName);
     assertEquals(langFR, language);
 
-    profile.setAttribute("user.language",langFR_FR);
-    profileHandler.saveUserProfile(profile,true);
+    profile.setAttribute("user.language", langFR_FR);
+    begin();
+    try {
+      profileHandler.saveUserProfile(profile, true);
+    } finally {
+      end();
+    }
 
     language = NotificationPluginUtils.getLanguage(userName);
     assertEquals(langFR_FR, language);
@@ -48,15 +63,15 @@ public class NotificationPluginUtilsTest extends TestCase {
 
   public void testGetBrandingPortalName() throws Exception {
     SettingService settingService = Mockito.mock(SettingService.class);
-    PortalContainer container = PortalContainer.getInstance();
-    container.unregisterComponent(SettingService.class);
-    container.registerComponentInstance(SettingService.class, settingService);
+    getContainer().unregisterComponent(SettingService.class);
+    getContainer().registerComponentInstance(SettingService.class, settingService);
 
     String companyName = "ACME";
     SettingValue value = new SettingValue(companyName);
     Mockito.when(settingService.get(Context.GLOBAL, Scope.GLOBAL, BRANDING_COMPANY_NAME_SETTING_KEY)).thenReturn(value);
-    
-    // Make sure that method getBrandingPortalName returns the changed company name.
+
+    // Make sure that method getBrandingPortalName returns the changed company
+    // name.
     assertEquals(companyName, NotificationPluginUtils.getBrandingPortalName());
   }
 }
