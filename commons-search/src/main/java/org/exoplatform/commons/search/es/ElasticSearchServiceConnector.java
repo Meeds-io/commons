73ae8cc847278c3d4f4f966153e05a8539a551eb
Azmi touil <atouil@exoplatform.com>
2020-07-15 16:45:10 +0100

    Revert code

--- commons-search/src/main/java/org/exoplatform/commons/search/es/ElasticSearchServiceConnector.java
+++ commons-search/src/main/java/org/exoplatform/commons/search/es/ElasticSearchServiceConnector.java
@@ -193,10 +193,7 @@ public class ElasticSearchServiceConnector extends SearchServiceConnector {
     if (StringUtils.isNotBlank(query)) {
       List<String> queryParts = Arrays.asList(query.split(" "));
       queryParts = queryParts.stream().map(queryPart -> {
-        queryPart = "\\\"" +  this.escapeReservedCharacters(queryPart) + "\\\"";
-        if (queryPart.length() > 5) {
-          queryPart = queryPart + "~1"; // fuzzy search on big words
-        }
+        queryPart = this.escapeReservedCharacters(queryPart);
         return queryPart;
       }).collect(Collectors.toList());
       String escapedQueryWithAndOperator = StringUtils.join(queryParts, " AND ");
