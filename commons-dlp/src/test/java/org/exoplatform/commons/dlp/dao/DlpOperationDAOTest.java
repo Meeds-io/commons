e49ed7dc016a596ff783b776e08945a6195549a1
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2020-12-21 17:36:10 +0100

    Task-38223: Implement Dlp feature V0 unit tests (#50)

---
+++ commons-dlp/src/test/java/org/exoplatform/commons/dlp/dao/DlpOperationDAOTest.java
@@ -0,0 +1,98 @@
+package org.exoplatform.commons.dlp.dao;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.lang.reflect.Field;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.exoplatform.commons.dlp.domain.DlpOperation;
+import org.exoplatform.container.PortalContainer;
+
+public class DlpOperationDAOTest extends AbstractDAOTest {
+
+  private DlpOperationDAO dlpOperationDAO;
+
+  @Before
+  public void setUp() {
+    PortalContainer container = PortalContainer.getInstance();
+    dlpOperationDAO = container.getComponentInstanceOfType(DlpOperationDAO.class);
+  }
+
+  @After
+  public void tearDown() {
+    dlpOperationDAO.deleteAll();
+  }
+
+  @Test
+  public void testDlpQueueCreation() throws IllegalAccessException, NoSuchFieldException {
+
+    //Given
+    List<DlpOperation> dlpOperations = dlpOperationDAO.findAll();
+    assertEquals(dlpOperations.size(), 0);
+
+    //When
+    createDlpOperations();
+
+    //Then
+    assertEquals(dlpOperationDAO.findAll().size(), 3);
+    dlpOperations = dlpOperationDAO.findAll();
+    Field privateField = DlpOperation.class.getDeclaredField("timestamp");
+    privateField.setAccessible(true);
+    Date timestamp = (Date) privateField.get(dlpOperations.get(0));
+    assertNull(timestamp);
+  }
+  
+  @Test
+  public void testFindByEntityIdAndType() {
+
+    //Given
+    assertEquals(dlpOperationDAO.findAll().size(), 0);
+    assertEquals(dlpOperationDAO.findByEntityIdAndType("1", "file").size(), 0);
+    assertEquals(dlpOperationDAO.findByEntityIdAndType("100", "file").size(), 0);
+
+    //When
+    createDlpOperations();
+
+    //Then
+    assertEquals(dlpOperationDAO.findAll().size(), 3);
+    assertEquals(dlpOperationDAO.findByEntityIdAndType("1", "file").size(), 1);
+    assertEquals(dlpOperationDAO.findByEntityIdAndType("100", "file").size(), 0);
+  }
+  
+  @Test
+  public void testFindAllFirst() {
+
+    //Given
+    assertEquals(dlpOperationDAO.findAll().size(), 0);
+    assertEquals(dlpOperationDAO.findAllFirst(2).size(), 0);
+
+    //When
+    createDlpOperations();
+
+    //Then
+    assertEquals(dlpOperationDAO.findAll().size(), 3);
+    assertEquals(dlpOperationDAO.findAllFirst(2).size(), 2);
+  }
+  
+  private void createDlpOperations () {
+    DlpOperation dlpOperation = new DlpOperation();
+    dlpOperation.setEntityType("file");
+    dlpOperation.setEntityId("1");
+    dlpOperationDAO.create(dlpOperation);
+    dlpOperation = new DlpOperation();
+    dlpOperation.setEntityType("file");
+    dlpOperation.setEntityId("22");
+    dlpOperationDAO.create(dlpOperation);
+    dlpOperation = new DlpOperation();
+    dlpOperation.setEntityType("activity");
+    dlpOperation.setEntityId("100");
+    dlpOperationDAO.create(dlpOperation);
+  }
+}
+
---
+++ commons-dlp/src/test/java/org/exoplatform/commons/dlp/processor/DlpOperationProcessorTest.java
@@ -0,0 +1,110 @@
+package org.exoplatform.commons.dlp.processor;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import org.exoplatform.commons.dlp.connector.DlpServiceConnector;
+import org.exoplatform.commons.dlp.dao.DlpOperationDAO;
+import org.exoplatform.commons.dlp.domain.DlpOperation;
+import org.exoplatform.commons.dlp.processor.impl.DlpOperationProcessorImpl;
+import org.exoplatform.container.PortalContainer;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DlpOperationProcessorTest {
+
+  private DlpOperationProcessorImpl dlpOperationProcessor;
+
+  @Mock
+  private DlpOperationDAO dlpOperationDAO;
+  
+  @Mock
+  private DlpServiceConnector dlpFileServiceConnector;
+  
+  @Mock
+  private DlpServiceConnector dlpActivityServiceConnector;
+
+  @Before
+  public void initMocks() {
+    MockitoAnnotations.initMocks(this);
+
+    // Make sure a portal container is started
+    PortalContainer.getInstance();
+    initDlpServiceConnector();
+    dlpOperationProcessor = new DlpOperationProcessorImpl(dlpOperationDAO);
+  }
+
+  private void initDlpServiceConnector() {
+    when(dlpFileServiceConnector.getType()).thenReturn("file");
+    when(dlpFileServiceConnector.getDisplayName()).thenReturn("file");
+    when(dlpFileServiceConnector.isEnable()).thenReturn(true);
+    
+    when(dlpActivityServiceConnector.getType()).thenReturn("activity");
+    when(dlpActivityServiceConnector.getDisplayName()).thenReturn("activity");
+    when(dlpActivityServiceConnector.isEnable()).thenReturn(true);
+  }
+
+  @After
+  public void clean() {
+    dlpOperationProcessor.getConnectors().clear();
+  }
+
+  @Test
+  public void testAddConnector() {
+    //Given
+    assertEquals(0, dlpOperationProcessor.getConnectors().size());
+    //When
+    dlpOperationProcessor.addConnector(dlpFileServiceConnector);
+    //Then
+    assertEquals(1, dlpOperationProcessor.getConnectors().size());
+  }
+  
+  @Test
+  public void testProcess() {
+    //Given
+    dlpOperationProcessor.start();
+    dlpOperationProcessor.addConnector(dlpFileServiceConnector);
+    dlpOperationProcessor.addConnector(dlpActivityServiceConnector);
+    when(dlpOperationDAO.findAllFirst(anyInt())).thenReturn(getDlpOperations());
+    when(dlpFileServiceConnector.processItem(anyString())).thenReturn(true);
+    when(dlpActivityServiceConnector.processItem(anyString())).thenReturn(true);
+    
+    //When
+    dlpOperationProcessor.process();
+    
+    //Then
+    verify(dlpOperationDAO, times(3)).delete(any());
+  }
+  
+  private List<DlpOperation> getDlpOperations() {
+    List<DlpOperation> dlpOperations = new ArrayList<DlpOperation>();
+    DlpOperation dlpOperation = new DlpOperation();
+    dlpOperation.setId(1L);
+    dlpOperation.setEntityType("file");
+    dlpOperation.setEntityId("1");
+    dlpOperations.add(dlpOperation);
+    dlpOperation = new DlpOperation();
+    dlpOperation.setId(2L);
+    dlpOperation.setEntityType("file");
+    dlpOperation.setEntityId("22");
+    dlpOperations.add(dlpOperation);
+    dlpOperation = new DlpOperation();
+    dlpOperation.setId(3L);
+    dlpOperation.setEntityType("activity");
+    dlpOperation.setEntityId("100");
+    dlpOperations.add(dlpOperation);
+    return dlpOperations;
+  }
+  
+}
