940aa3e15f580785ca68bfdcedb94a9d554116f3
ahmed <achakroun@exoplatform.com>
2021-02-19 16:29:21 +0100

    Task-40603 :  Implement notifications for users who have access to quarantine feature

--- commons-dlp/src/main/java/org/exoplatform/commons/dlp/service/impl/DlpPermissionsServiceImpl.java
+++ commons-dlp/src/main/java/org/exoplatform/commons/dlp/service/impl/DlpPermissionsServiceImpl.java
@@ -1,7 +1,12 @@
 package org.exoplatform.commons.dlp.service.impl;
 
+import org.apache.commons.lang.StringUtils;
+import org.exoplatform.commons.api.settings.SettingService;
+import org.exoplatform.commons.api.settings.SettingValue;
 import org.exoplatform.commons.dlp.service.DlpPermissionsService;
+import org.exoplatform.commons.utils.CommonsUtils;
 import org.exoplatform.portal.config.DataStorage;
+import org.exoplatform.portal.config.UserACL;
 import org.exoplatform.portal.config.model.*;
 import org.exoplatform.portal.mop.SiteKey;
 import org.exoplatform.portal.mop.SiteType;
@@ -12,9 +17,15 @@ import org.exoplatform.portal.mop.page.PageService;
 import org.exoplatform.portal.mop.page.PageState;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
+import org.exoplatform.services.organization.Membership;
+import org.exoplatform.services.organization.OrganizationService;
+import org.exoplatform.services.security.ConversationState;
+import org.exoplatform.services.security.IdentityRegistry;
 
+import javax.xml.ws.handler.MessageContext;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class DlpPermissionsServiceImpl implements DlpPermissionsService {
@@ -42,6 +53,7 @@ public class DlpPermissionsServiceImpl implements DlpPermissionsService {
     //Load quarantine page and Application portlet
     List<String> permissionsList = Arrays.asList(permissions.split(","));
     for (String permission : permissionsList) {
+      if(permission.equals("/platform/administrators")) continue;
       Page dlpPage = new Page();
       dlpPage.setOwnerType(PortalConfig.GROUP_TYPE);
       dlpPage.setOwnerId(permission);
@@ -94,6 +106,7 @@ public class DlpPermissionsServiceImpl implements DlpPermissionsService {
   public void removeDlpPermissionsPagesAndNavigations(String oldPermissions) {
     List<String> permissionsList = Arrays.asList(oldPermissions.split(","));
     for (String permission : permissionsList){
+      if(permission.equals("/platform/administrators")) continue;
       NavigationContext nav = navigationService.loadNavigation(SiteKey.group(permission));
       if (nav != null) {
         NodeContext dlpPermissionNodeContext = navigationService.loadNode(NodeModel.SELF_MODEL, nav, Scope.ALL, null);
@@ -108,5 +121,40 @@ public class DlpPermissionsServiceImpl implements DlpPermissionsService {
         }
       }
     }
+  }
+
+  @Override
+  public String getDlpQuarantinePageUrl(String username) {
+    IdentityRegistry identityRegistry = CommonsUtils.getService(IdentityRegistry.class);
+    OrganizationService organizationService = CommonsUtils.getService(OrganizationService.class);
+    SettingService settingService = CommonsUtils.getService(SettingService.class);
+    UserACL userACL = CommonsUtils.getService(UserACL.class);
+    if (StringUtils.isBlank(username)) {
+      return new String();
+    }
+    if(userACL.isSuperUser() || userACL.isUserInGroup(userACL.getAdminGroups())){
+      return "/g/:platform:administrators/" + DLP_QUARANTINE;
+    }
+    SettingValue<?> settingValue = settingService.get(org.exoplatform.commons.api.settings.data.Context.GLOBAL,
+                           org.exoplatform.commons.api.settings.data.Scope.APPLICATION.id("DlpPermissions"),
+                           "exo:dlpPermissions");
+    if (settingValue == null || settingValue.getValue().toString().isEmpty()) return new String();
+    List<String> permissionsList = Arrays.asList(settingValue.getValue().toString().split(","));
+    for (String permissionExpression : permissionsList) {
+      org.exoplatform.services.security.Identity identity = identityRegistry.getIdentity(username);
+      if (identity != null && identity.isMemberOf(permissionExpression)) {
+        return "/g/" + permissionExpression.replaceAll("/", ":") + "/" + DLP_QUARANTINE;
+      }
+      try {
+        Collection<Membership> memberships = organizationService.getMembershipHandler()
+                                                                .findMembershipsByUserAndGroup(username, permissionExpression);
+        if (memberships != null && !memberships.isEmpty()) {
+          return "/g/" + permissionExpression.replaceAll("/", ":") + "/" + DLP_QUARANTINE;
+        }
+      } catch (Exception e) {
+        throw new IllegalStateException("Error getting memberships of user " + username, e);
+      }
+    }
+    return new String();
   }
 }
