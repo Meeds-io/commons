3c01576e72c7330e95339f61c7a4e9ffdb248565
Ahmed <achakroun@exoplatform.com>
2021-08-04 16:10:22 +0100

    Task-46504 : AdminMFA protected resources navigations (#200)

---
+++ commons-mfa/src/main/java/org/exoplatform/mfa/api/MfaNavigations.java
@@ -0,0 +1,15 @@
+package org.exoplatform.mfa.api;
+
+public class MfaNavigations {
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  private String id;
+
+}
