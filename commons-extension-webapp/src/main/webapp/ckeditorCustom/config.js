fa17608f4bac37e240e710956ead3fec22719252
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-02-25 18:48:22 +0100

    Move CKEditor File Attachment Custom Plugin to ECMS (#291)
    This PR will move CKEditor File Attachment Custom Plugin to ECMS

--- commons-extension-webapp/src/main/webapp/ckeditorCustom/config.js
+++ commons-extension-webapp/src/main/webapp/ckeditorCustom/config.js
@@ -32,9 +32,8 @@ CKEDITOR.editorConfig = function( config ) {
     CKEDITOR.plugins.addExternal('autoembed','/commons-extension/eXoPlugins/autoembed/','plugin.js');
     CKEDITOR.plugins.addExternal('embedsemantic','/commons-extension/eXoPlugins/embedsemantic/','plugin.js');
     CKEDITOR.plugins.addExternal('tagSuggester','/commons-extension/eXoPlugins/tagSuggester/','plugin.js');
-    CKEDITOR.plugins.addExternal('attachFile','/commons-extension/eXoPlugins/attachFile/','plugin.js');
 
-    config.extraPlugins = 'simpleLink,selectImage,suggester,hideBottomToolbar,attachFile';
+    config.extraPlugins = 'simpleLink,selectImage,suggester,hideBottomToolbar';
     config.skin = 'moono-exo,/commons-extension/ckeditor/skins/moono-exo/';
     // %REMOVE_END%
 
