cac5d48cec3ee97a30e9fc12540dff50cb7687e7
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2022-05-09 17:26:31 +0200

    Task-54013: Move dlp commons to dlp addon codebase

--- commons-dlp/src/main/java/org/exoplatform/commons/dlp/dao/RestoredDlpItemDAO.java
+++
@@ -1,9 +0,0 @@
-package org.exoplatform.commons.dlp.dao;
-
-import org.exoplatform.commons.api.persistence.GenericDAO;
-import org.exoplatform.commons.dlp.domain.RestoredDlpItemEntity;
-
-public interface RestoredDlpItemDAO extends GenericDAO<RestoredDlpItemEntity, Long> {
-
-  RestoredDlpItemEntity findRestoredDlpItemByReference(String itemReference);
-}
