2acc32d2e7b2d3b22deb61b8f5e112c3bb508f46
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-07-04 13:26:31 +0100

    MEED-196 Fix version comparaison when it contains 0 at the end Prior to this change, the 0 suffix inside a version was removed and thus, the SNAPSHOT versions was considered as always after the GA version ( by example 6.4.0 ). This change will fix the versions comparaison by not removing the suffixed 0. On the other hand, the non-compatible versions will not be comparable like: 1.0 is not the same as 1.0.0. (which is logic when we use the same versioning system for the same product)

--- commons-component-upgrade/src/test/java/org/exoplatform/commons/version/util/VersionComparatorTest.java
+++ commons-component-upgrade/src/test/java/org/exoplatform/commons/version/util/VersionComparatorTest.java
@@ -10,7 +10,6 @@ public class VersionComparatorTest extends TestCase {
     assertFalse(VersionComparator.isBefore("2.1", "2.1"));
     assertFalse(VersionComparator.isBefore("2.1", ""));
     assertTrue(VersionComparator.isBefore("", "2.2"));
-    assertTrue(VersionComparator.isBefore("5.0.0-M32", "5.0-RC1"));
     assertTrue(VersionComparator.isBefore("6.2.0-20210529", "6.2.0-20210531"));
     assertFalse(VersionComparator.isBefore("6.2.0-20210601", "6.2.0-20210531"));
     assertTrue(VersionComparator.isBefore("6.1.1", "6.2.0-20210531"));
@@ -29,6 +28,11 @@ public class VersionComparatorTest extends TestCase {
     assertTrue(VersionComparator.isBefore("6.2.0-20210531", "6.2.0-rc01"));
     assertTrue(VersionComparator.isBefore("6.2.0-rc02", "6.2.0-rc03"));
     assertTrue(VersionComparator.isBefore("6.2.0-m15", "6.2.0-rc03"));
+    assertTrue(VersionComparator.isBefore("6.4.x", "6.4.0"));
+    assertTrue(VersionComparator.isBefore("6.4.x", "6.4.1"));
+    assertTrue(VersionComparator.isBefore("6.4.x-SNAPSHOT", "6.4.0"));
+    assertTrue(VersionComparator.isBefore("6.4.x-SNAPSHOT", "6.4.1"));
+    assertTrue(VersionComparator.isBefore("6.4.x-SNAPSHOT-rev20220706", "6.4.x-SNAPSHOT-rev20220707"));
   }
 
   public void testIsAfter() {
