36194aa55f49cb562e9a94a40783ca05bcb6ede1
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2022-08-24 17:00:12 +0100

    MEED-310: Add Event listener when we click to open editor format options (#385)

--- commons-extension-webapp/src/main/webapp/eXoPlugins/formatOption/plugin.js
+++ commons-extension-webapp/src/main/webapp/eXoPlugins/formatOption/plugin.js
@@ -9,6 +9,7 @@ CKEDITOR.plugins.add( 'formatOption', {
       exec: function() {
         const toolbarWrapper = document.getElementsByClassName("cke_toolgroup");
         toolbarWrapper[0].classList.toggle("fullToolbar");
+        document.dispatchEvent(new CustomEvent('editors-options-opened'));
       }
     });
 
