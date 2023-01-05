cc817690d89b0d0993c82731357edde56d70f1fb
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-01-05 11:47:17 +0100

    feat: Cleanup Old Code and Apply changes due to Test optimizations Made on Portal - MEED-1451 - Meeds-io/MIPs#42 (#437)
    Prior to this change some useless code was remaining while its usage was
    removed from Acitity preview and used only for notification preview.
    This change will delete the embed images generation that leaks
    information from outside infrastructure to get an image preview to put
    into notification. Same as Search Indexing Portlet which is not used
    anymore, and even dangerous knowing that the ElasticSearch data is not
    re-genrated anymore by connectors.

--- commons-api/src/main/java/org/exoplatform/commons/api/indexing/data/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides the classes that defines pattern structures of data entry. Connector must follow to it during the implementation process 
- */
-package org.exoplatform.commons.api.indexing.data;
