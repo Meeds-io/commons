58f5643db6e345b9caea9d9fbdddc2e94a52fd74
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-01-26 09:21:01 +0100

    fix: Change irrelevant log warn to info about used default value for properties and delete useless favicon file - Meeds-io/meeds#601 (#446)
    This will change an irrelevant `LOG.warn` to `LOG.info`.
    At the same time, this will cleanup a useless favicon file.

--- commons-search/src/main/java/org/exoplatform/commons/search/es/client/ElasticSearchingClient.java
+++ commons-search/src/main/java/org/exoplatform/commons/search/es/client/ElasticSearchingClient.java
@@ -129,8 +129,9 @@ public class ElasticSearchingClient extends ElasticClient {
         maxPoolConnections = Integer.parseInt(maxConnectionsValue.trim());
       }
       if (maxPoolConnections <= 0) {
-        LOG.warn(ES_SEARCH_CLIENT_PROPERTY_MAX_CONNECTIONS + " value is not a positive number : " + maxConnectionsValue +
-            ". Using default HTTP max connections (" + DEFAULT_MAX_HTTP_POOL_CONNECTIONS + ").");
+        LOG.info("Using default HTTP max connections for property {}={}.",
+                 ES_SEARCH_CLIENT_PROPERTY_MAX_CONNECTIONS,
+                 DEFAULT_MAX_HTTP_POOL_CONNECTIONS);
         maxPoolConnections = DEFAULT_MAX_HTTP_POOL_CONNECTIONS;
       }
     }
