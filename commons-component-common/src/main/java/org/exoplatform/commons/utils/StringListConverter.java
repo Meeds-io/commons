ed03482cb198ab37ab03850d8240bf42250b379e
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2023-09-12 15:41:12 +0200

    feat: Convert list in entity to single string column in database - MEED-2301 - Meeds-io/MIPs#64 (#510)
    This PR will add a standard class to convert a list in an entity to a
    single string column in the database

---
+++ commons-component-common/src/main/java/org/exoplatform/commons/utils/StringListConverter.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package org.exoplatform.commons.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class StringListConverter implements AttributeConverter<List<String>, String> {
+
+  private static final String DELIMITER = ",";
+
+  @Override
+  public String convertToDatabaseColumn(List<String> attribute) {
+    return attribute != null ? String.join(DELIMITER, attribute) : null;
+  }
+
+  @Override
+  public List<String> convertToEntityAttribute(String dbData) {
+    return dbData != null ? Arrays.stream(dbData.split(DELIMITER)).toList() : null;
+  }
+}
