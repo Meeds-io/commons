1f6e98e2b69c387361a79c60f28286fde624248b
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2023-09-15 18:06:31 +0100

    feat: Add ckeditor codesnippets, table, copyFormatting and link plugins- EXO-65695 - Meeds-io/MIPs#71 (#514)
    Add ckeditor codesnippets, table, copyFormatting and link plugins

---
+++ commons-extension-webapp/src/main/webapp/ckeditor/plugins/codesnippet/lang/eo.js
@@ -0,0 +1,13 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * CKEditor 4 LTS ("Long Term Support") is available under the terms of the Extended Support Model.
+ */
+
+CKEDITOR.plugins.setLang( 'codesnippet', 'eo', {
+	button: 'Enmeti kodaĵeron',
+	codeContents: 'Kodenhavo',
+	emptySnippetError: 'Kodaĵero ne povas esti malplena.',
+	language: 'Lingvo',
+	title: 'Kodaĵero',
+	pathName: 'kodaĵero'
+} );
