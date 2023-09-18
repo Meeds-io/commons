3cf5e0dce769d395b727e2ab2408f36384010a0f
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2023-09-18 12:03:52 +0100

    feat: Add ckeditor colorDialog plugin - EXO-65704 - Meeds-io/MIPs#71 (#518)
    Add ckeditor colorDialog plugin

---
+++ commons-extension-webapp/src/main/webapp/ckeditor/plugins/colordialog/lang/et.js
@@ -0,0 +1,11 @@
+/*
+Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+CKEditor 4 LTS ("Long Term Support") is available under the terms of the Extended Support Model.
+*/
+CKEDITOR.plugins.setLang( 'colordialog', 'et', {
+	clear: 'Eemalda',
+	highlight: 'Näidis',
+	options: 'Värvi valikud',
+	selected: 'Valitud värv',
+	title: 'Värvi valimine'
+} );
