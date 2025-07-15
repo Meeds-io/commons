777496ab0b77f97a13e34805d5ba1f46944531d0
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-07-15 09:29:59 +0100

    Add Java and XML Files License Headers - Meeds-io/meeds#3324 (#707)
    This change will add LGPL License Header and clean useless File Headers.

--- commons-component-common/src/main/java/org/exoplatform/commons/notification/net/WebNotificationSender.java
+++ commons-component-common/src/main/java/org/exoplatform/commons/notification/net/WebNotificationSender.java
@@ -1,18 +1,20 @@
-/*
- * Copyright (C) 2003-2014 eXo Platform SAS.
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
  *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package org.exoplatform.commons.notification.net;
 
@@ -24,12 +26,6 @@ import org.exoplatform.ws.frameworks.cometd.ContinuationService;
 import org.exoplatform.ws.frameworks.json.impl.JsonGeneratorImpl;
 import org.exoplatform.ws.frameworks.json.value.JsonValue;
 
-/**
- * Created by The eXo Platform SAS
- * Author : eXoPlatform
- *          exo@exoplatform.com
- * Nov 23, 2014  
- */
 public class WebNotificationSender {
   private final static Log LOG = ExoLogger.getLogger(WebNotificationSender.class);
   private final static String COMETD_CHANNEL = "/eXo/Application/web/NotificationMessage";
--- commons-component-common/src/main/java/org/exoplatform/services/bench/LoremIpsum4J.java
+++ commons-component-common/src/main/java/org/exoplatform/services/bench/LoremIpsum4J.java
@@ -1,40 +1,21 @@
-/* Copyright (c) 2008 Sven Jacobs
-
-
-   Permission is hereby granted, free of charge, to any person obtaining a copy
-
-   of this software and associated documentation files (the "Software"), to deal
-
-   in the Software without restriction, including without limitation the rights
-
-   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-
-   copies of the Software, and to permit persons to whom the Software is
-
-   furnished to do so, subject to the following conditions:
-
-
-   The above copyright notice and this permission notice shall be included in
-
-   all copies or substantial portions of the Software.
-
-
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-
-   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-
-   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-
-   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-
-   THE SOFTWARE. 
-
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-
 package org.exoplatform.services.bench;
 
 /**
@@ -43,8 +24,6 @@ package org.exoplatform.services.bench;
  * Suitable for creating sample data for test cases and performance tests.
  * </p>
  * 
- * @author Sven Jacobs
- * @version 1.0
  */
 
 public class LoremIpsum4J {
