cd80c3cd2482bb8a2e2184abb1ea379c6be65e7c
Ali Hamdi <ahamdi@exoplatform.com>
2025-12-11 12:33:06 +0100

    fix: delete old notifications successfully - MEED-9976

--- commons-component-common/src/main/java/org/exoplatform/commons/notification/impl/jpa/web/entity/WebNotifEntity.java
+++ commons-component-common/src/main/java/org/exoplatform/commons/notification/impl/jpa/web/entity/WebNotifEntity.java
@@ -22,15 +22,7 @@ import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity(name = "NotificationsWebNotifEntity")
 @Table(name = "NTF_WEB_NOTIFS")
@@ -53,7 +45,7 @@ public class WebNotifEntity {
   @Column(name = "TEXT")
   private String text;
 
-  @OneToMany(fetch=FetchType.EAGER, mappedBy = "webNotification")
+  @OneToMany(fetch=FetchType.EAGER, cascade = CascadeType.ALL,  mappedBy = "webNotification")
   private Set<WebParamsEntity> parameters = new HashSet<>();
 
   @OneToMany(fetch=FetchType.LAZY, mappedBy = "webNotification")
