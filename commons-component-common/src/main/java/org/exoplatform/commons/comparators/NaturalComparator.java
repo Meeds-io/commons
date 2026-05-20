495f1f2c3f505534f853ba964034f05b80398e0f
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2026-05-20 11:01:03 +0200

    fix: improve NaturalComparator with accent support and Locale customization - MEED-10342 - Meeds-io/meeds#4154
    This change will enhance NaturalComparator for accent-aware sorting.
    
    (cherry picked from commit eb2c948ba82be0f37c29c2f2528cc9edbf0b2b7d)

--- commons-component-common/src/main/java/org/exoplatform/commons/comparators/NaturalComparator.java
+++ commons-component-common/src/main/java/org/exoplatform/commons/comparators/NaturalComparator.java
@@ -21,8 +21,22 @@ package org.exoplatform.commons.comparators;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Comparator;
+import java.text.Collator;
+import java.util.Locale;
 
 public class NaturalComparator implements Comparator<String> {
+
+  private final Collator collator;
+
+  public NaturalComparator() {
+    this(Locale.getDefault());
+  }
+
+  public NaturalComparator(Locale locale) {
+    this.collator = Collator.getInstance(locale);
+    this.collator.setStrength(Collator.SECONDARY); // Accent-sensitive, case-insensitive
+  }
+
   @Override
   public int compare(String s1, String s2) {
       /*
@@ -30,30 +44,32 @@ public class NaturalComparator implements Comparator<String> {
       (?<=) matches the position immediately following a non-digit (\\D) or a digit (\\d) character
       (?=) matches the position immediately preceding a non-digit (\\D) or a digit (\\d) character
        */
-    if(s1 == null) {
+    if (s1 == null) {
       return 1;
     }
-    if(s2 == null) {
+    if (s2 == null) {
       return -1;
     }
     String[] arr1 = s1.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
     String[] arr2 = s2.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
     int i = 0;
     while (i < arr1.length && i < arr2.length) {
-      if (StringUtils.isNotBlank(arr1[i]) && Character.isDigit(arr1[i].charAt(0)) && StringUtils.isNotBlank(arr2[i]) && Character.isDigit(arr2[i].charAt(0))) {
+      if (StringUtils.isNotBlank(arr1[i]) && Character.isDigit(arr1[i].charAt(0)) &&
+              StringUtils.isNotBlank(arr2[i]) && Character.isDigit(arr2[i].charAt(0))) {
+
         int num1 = Integer.parseInt(arr1[i]);
         int num2 = Integer.parseInt(arr2[i]);
         if (num1 != num2) {
           return Integer.compare(num1, num2);
         }
       } else {
-        int result = arr1[i].compareToIgnoreCase(arr2[i]);
+        int result = collator.compare(arr1[i], arr2[i]);
         if (result != 0) {
           return result;
         }
       }
       i++;
     }
-    return s1.compareToIgnoreCase(s2);
+    return collator.compare(s1, s2);
   }
 }
